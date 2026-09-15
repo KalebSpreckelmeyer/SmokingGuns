6f5a215de9e69fad9ca478da0116c74fc63406f2
Kaleb Spreckelmeyer <kcs24833@email.vccs.edu>
2026-09-15 03:11:29 -0500

    Rename project to Smoking Guns

--- xmake.lua
+++ xmake.lua
@@ -2,8 +2,8 @@
 includes("lib/commonlibf4")
 
 -- set project constants
-set_project("commonlibf4-template")
-set_version("0.0.0")
+set_project("SmokingGuns")
+set_version("0.1.0")
 set_license("GPL-3.0")
 set_languages("c++23")
 set_warnings("allextra")
@@ -13,15 +13,17 @@ add_rules("mode.debug", "mode.releasedbg")
 add_rules("plugin.vsxmake.autoupdate")
 
 -- define targets
-target("commonlibf4-template")
+target("SmokingGuns")
+
     add_rules("commonlibf4.plugin", {
-        name = "commonlibf4-template",
-        author = "libxse",
-        description = "F4SE plugin template using CommonLibF4"
+        name = "Smoking Guns",
+        author = "Kaleb Spreckelmeyer",
+        description = "Smoking Guns framework for Fallout 4"
     })
 
     -- add src files
     add_files("src/**.cpp")
     add_headerfiles("src/**.h")
     add_includedirs("src")
+
     set_pcxxheader("src/pch.h")
